6fedb5e1a1bb846bcdf6a241db883a909bc28fd6
rohitreddyeipimedia <155817010+rohitreddyeipimedia@users.noreply.github.com>
2026-02-23 10:05:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['images.unsplash.com', 'cdn.openai.com', 'blog.openai.com', 'arxiv.org'],
   },
